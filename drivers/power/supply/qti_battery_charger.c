fa9bc72beec89df804efcf7562843a99791c3a55
TheStrechh <TheStrechh@gmail.com>
2023-07-21 23:58:33 +0530

    fixup! power: supply: qti_battery_charger: Return charge counter in uAh
    This was necessary to show proper battery stats on devices like lisa/haydn/renoir, refer to this for more details:
    https://github.com/LineageOS/android_kernel_xiaomi_sm8350/commit/690e81a4109b6f8702589342972bbc07ead48fd6
    
    Devices using BQ_FUEL_GAUGE seem to report this value correctly by default, so exclude the patch for them.

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -1679,8 +1679,7 @@ static int battery_psy_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX:
 		pval->intval = bcdev->num_thermal_levels;
 		break;
-#ifdef CONFIG_MACH_XIAOMI
-	// Ghost-riderreborn workaround for charge counter
+#if !defined(CONFIG_BQ_FUEL_GAUGE)
 	case POWER_SUPPLY_PROP_CHARGE_COUNTER:
 		pval->intval = DIV_ROUND_CLOSEST(pst->prop[prop_id], 100);
 		break;
